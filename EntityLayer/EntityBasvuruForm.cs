fc1176601e8a86e8c0a425b2d904b76646f552db
Seymanur <eynanur18@gmail.com>
2022-09-27 14:06:28 +0300

    Add project files.

---
+++ EntityLayer/EntityBasvuruForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class EntityBasvuruForm
+    {
+        private int basvuruid;
+        private int basdersid;
+        private int basogrid;
+
+        public int Basvuruid { get { return basvuruid; } set { basvuruid = value; } }
+        public int Basdersid { get { return basdersid; } set { basdersid = value; } }
+        public int Basogrid { get { return basogrid; } set { basogrid = value; } }
+    }
+}
---
+++ EntityLayer/EntityOgretmen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class EntityOgretmen
+    {
+        private int ogrtid;
+        private string ogrtad;
+        private int ogrtbrans;
+
+        public int OGRTID { get { return ogrtid; } set { ogrtid = value; } }
+        public string OGRTAD { get { return ogrtad; } set { ogrtad = value; } }
+        public int OGRTBRANS { get { return ogrtbrans; } set { ogrtbrans = value; } }
+    }
+}
